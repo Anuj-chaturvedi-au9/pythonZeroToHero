32d362abacbea1e77d5a2e86ea198047a019abf4
Anuj-chaturvedi-au9 <67587859+Anuj-chaturvedi-au9@users.noreply.github.com>
2021-07-28 17:37:40 +0530

    Add files via upload

---
+++ day01.py
@@ -0,0 +1,6 @@
+someValues = [int(i) for i in input("enter some values : ").split()]
+print("original list : ",someValues)
+someValues.sort()
+print("sorted values : ",someValues)
+someValues.reverse()
+print("reverse values",someValues)
